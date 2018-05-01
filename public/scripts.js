88e24cef2e1bca04425bbef25e949c91e8bc59c2
Ngoc Phan <nphan4@yahoo.com>
2018-04-30 19:02:42 -0600

    lock function done

--- public/scripts.js
+++ public/scripts.js
@@ -1,16 +1,18 @@
 $('document').ready(setRandomColor);
-
 $('.generate-button').on('click', setRandomColor);
+$('.unlocked').on('click', toggleLock);
 
 function setRandomColor () {
   const colorPalatte = [$('.box-one'), $('.box-two'), $('.box-three'), $('.box-four'), $('.box-five')];
   
-  colorPalatte.forEach((color, i) => {
+  colorPalatte.forEach(color => {
+    if (!color.children('img').hasClass('locked')) { 
     let randomColorHex = generateRandomColor();
+
 
     color.siblings('p').text(randomColorHex);
     color.css('background-color', randomColorHex);
-  });
+  }})
 }
 
 function generateRandomColor () {
@@ -23,3 +25,9 @@ function generateRandomColor () {
   return hex;
 };
 
+function toggleLock () {
+  $(this).toggleClass('locked');
+  let src = ($(this).attr('src') === './assets/unlock.svg') ? './assets/locked.svg' : './assets/unlock.svg'
+
+  $(this).attr('src', src);
+}
