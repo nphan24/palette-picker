0833a6be90d977387e24c5c306484d97f725cb56
Ngoc Phan <nphan4@yahoo.com>
2018-05-01 12:48:36 -0600

    Appended palette name

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,7 @@
 $('document').ready(setRandomColor);
 $('.generate-button').on('click', setRandomColor);
 $('.unlocked').on('click', toggleLock);
+$('.save-button').on('click', savePalette);
 
 function setRandomColor () {
   const colorPalatte = [$('.box-one'), $('.box-two'), $('.box-three'), $('.box-four'), $('.box-five')];
@@ -8,7 +9,6 @@ function setRandomColor () {
   colorPalatte.forEach(color => {
     if (!color.children('img').hasClass('locked')) { 
     let randomColorHex = generateRandomColor();
-
 
     color.siblings('p').text(randomColorHex);
     color.css('background-color', randomColorHex);
@@ -30,4 +30,12 @@ function toggleLock () {
   let src = ($(this).attr('src') === './assets/unlock.svg') ? './assets/locked.svg' : './assets/unlock.svg'
 
   $(this).attr('src', src);
+}
+
+function savePalette (event) {
+  event.preventDefault();
+  let paletteInput = $('.palette-input').val();
+
+  $('.saved-projects').append(`<h3>${paletteInput}</h3>`);
+  $('.palette-input').val('')
 }
