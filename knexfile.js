95725ba9eeb0da616ca38bb6f395679b7b998b77
Ngoc Phan <nphan4@yahoo.com>
2018-05-04 10:33:51 -0600

    Add Production to knex.file

--- knexfile.js
+++ knexfile.js
@@ -22,21 +22,13 @@ module.exports = {
       directory: './db/seeds/test'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 };
